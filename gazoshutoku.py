bc59bd78a262340e6b4146cc56ad7bec1bac01a1
ukitomo <tomoemon_u@outlook.jp>
2024-06-17 17:35:51 +0900

    バグを修正しソースコードを最適化しました

--- gazoshutoku.py
+++ gazoshutoku.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+#coding=utf-8
+import sys
 import time
 from datetime import datetime
 import os
@@ -11,42 +14,30 @@ url = None
 while not url:
     url = input("URLを入力してください")
 print(url)
+if not (url.startswith('https://') or url.startswith('http://')):
+    url = "http://" + url
 try:
     res = requests.get(url)
 except requests.exceptions.ConnectionError:
     print("接続に失敗しました")
-    done = False
-    answer = "n"
-except requests.exceptions.MissingSchema:
-    res = requests.get("http://" + url)
-    if res.status_code == requests.codes.ok:
-        done = True
-    else:
-        print("接続に失敗しました")
-        done = False
-        answer = "n"
+    sys.exit(1)
+if res.status_code != requests.codes.ok:
+    print("接続に失敗しました")
+    sys.exit(1)
+#セレクター指定
+selecter = None
+while not selecter:
+   selecter = input("CSSセレクターを入力してください")
+print(selecter)
+soup = BeautifulSoup(res.text, 'html.parser')
+pictures = soup.select(selecter)
+if pictures:
+    for pictureslist in pictures:
+        print(pictureslist)    
+    answer = input("これらの要素が" + str(len(pictures)) + "個見つかりましたダウンロードしますか?(y/n)")
 else:
-    if res.status_code == requests.codes.ok:
-        done = True
-    else:
-        print("接続に失敗しました")
-        done = False
-        answer = "n"
-if done:
-    #セレクター指定
-    selecter = None
-    while not selecter:
-       selecter = input("CSSセレクターを入力してください")
-    print(selecter)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    pictures = soup.select(selecter)
-    if pictures:
-        for pictureslist in pictures:
-            print(pictureslist)    
-        answer = input("これらの要素が" + str(len(pictures)) + "個見つかりましたダウンロードしますか?(y/n)")
-    else:
-        print("要素が見つかりませんでした")
-        answer = "n"
+    print("要素が見つかりませんでした")
+    sys.exit(1)
 if answer == "y":
     #フォルダ作成
     dt = datetime.now()
@@ -68,21 +59,24 @@ if answer == "y":
             url_rel = picture['src']
         except KeyError:
             print("src属性がありません")
-        else:
-            url_abs = urljoin(url, url_rel)
-            print(url_abs)
-            try:
-                img_res = requests.get(url_abs)
-            except requests.exceptions.ConnectionError:
-                print("接続に失敗しました")
-            else:
-                print(img_res)
-                if img_res.status_code == requests.codes.ok:
-                    img_name = Path(url_abs).name
-                    img_file_path = img_dir / img_name
-                    img_file_path.write_bytes(img_res.content)
-                    print(img_name)
-                else:
-                    print("ダウンロードに失敗しました")
+            time.sleep(1)
+            continue
+        url_abs = urljoin(url, url_rel)
+        print(url_abs)
+        try:
+            img_res = requests.get(url_abs)
+        except requests.exceptions.ConnectionError:
+            print("接続に失敗しました")
+            time.sleep(1)
+            continue                
+        print(img_res)
+        if img_res.status_code != requests.codes.ok:
+            print("ダウンロードに失敗しました")
+            time.sleep(1)
+            continue
+        img_name = Path(url_abs).name
+        img_file_path = img_dir / img_name
+        img_file_path.write_bytes(img_res.content)
+        print(img_name)     
         time.sleep(1)
 print("プログラムは終了しました")
